bf8300f60d2d13e7180daa7565096af4fe2ff064
Chris Miller <chris@djmultitouch.com>
2013-08-27 08:15:26 -0700

    Basic retro style and start of a JS app.  Feel free to reject this commit and do something different :)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,39 @@
-console.log('\'Allo \'Allo!');
+!function() {
+  var game = new function game()  {
+    var board = document.createElement('div'),
+        setup = !function setup() {
+          board.id = 'game';
+          document.body.appendChild(board);
+        }();
+
+    function makeEmptySeat() {
+      var el = document.createElement('span');
+      el.innerHTML = '[ ]';
+      return el; 
+    }
+
+    function makeFullSeat() {
+      var el = makeEmptySeat();
+      el.innerHTML = '[x]';
+      return el;
+    }
+
+    function makeRandomSeat() {
+      if (Math.floor(Math.random() * 2)) {
+        return makeFullSeat();
+      } else {
+        return makeEmptySeat();
+      }
+    }
+
+    this.start = function start() {
+      var empty, full = document.createElement('span'),
+          seatsToDraw = 10;;
+      while (seatsToDraw) {
+        board.appendChild(makeRandomSeat());
+        seatsToDraw--; 
+      }
+    };
+  };
+  game.start();
+}();
